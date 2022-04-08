4e42deae8c2300362a53356bc85a7273d99c19ba
aJPcreater <nagasawatatuya@gmail.com>
2022-04-09 00:19:27 +0900

    [□と○]発展課題2つ目を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,6 +1,25 @@
 // 図形の表示
 const figure = document.getElementById("figure")
 
-figure.onmouseover = function () {
-  figure.classList.toggle("rounded")
+let s = 0
+figure.onclick = function () {
+  s += 1
+  switch (s) {
+    case 0:
+      figure.classList.add("square")
+      figure.classList.remove("triangle")
+      break
+    case 1:
+      figure.classList.add("rounded")
+      break
+    case 2:
+      figure.classList.add("triangle")
+      figure.classList.remove("square")
+      figure.classList.remove("rounded")
+      s -= 3
+      break
+
+    default:
+      console.log("管理者用 : 発生し得ないexception")
+  }
 }
