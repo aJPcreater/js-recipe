a301c467e3b307d481bf8acdcec03d899bcd1255
aJPcreater <nagasawatatuya@gmail.com>
2022-04-09 08:14:16 +0900

    [カウンター]発展課題1つ目を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,7 +1,11 @@
 // display カウンター描画
 const display = document.getElementById("display")
-// plus-button カウント増やすボタン
+// plus-button １カウント増やすボタン
 const plusButton = document.getElementById("plus-button")
+// minus-button １カウント減らすボタン
+const minusButton = document.getElementById("minus-button")
+// double-button 2倍にするボタン
+const doubleButton = document.getElementById("double-button")
 
 let count = 0
 
@@ -9,5 +13,19 @@ plusButton.onclick = function () {
   // カウント上昇+1
   count += 1
   // 上昇後の count に書き換え
+  display.textContent = count
+}
+
+minusButton.onclick = function () {
+  // カウント減少-1
+  count -= 1
+  // 減少後の count に書き換え
+  display.textContent = count
+}
+
+doubleButton.onclick = function () {
+  // カウント*2
+  count *= 2
+  // 倍増後の count に書き換え
   display.textContent = count
 }
